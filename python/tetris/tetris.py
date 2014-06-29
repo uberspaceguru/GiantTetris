dccd7f1f52273028e52d04a018c58028f81d18ba
malloc2048 <jjmaroulis@gmail.com>
2014-06-29 19:51:56 -0400

    Catch Ctlr+C and turn on all pixels.

--- python/tetris/tetris.py
+++ python/tetris/tetris.py
@@ -32,15 +32,15 @@ while run:
         if led_y < 1:
             led_y = 20
 
-        #pixel_test = {'x': led_x, 'y': led_y, 'r': 1, 'g': 1, 'b': 1}
-        #pixel_test2 = {'x': 10, 'y': 20, 'r': 1, 'g': 1, 'b': 1}
-        #display.pixel_on(pixel=pixel_test)
-        # Enable following line to adda second active pixel
+        pixel_test = {'x': led_x, 'y': led_y, 'r': 1, 'g': 1, 'b': 1}
+        display.pixel_on(pixel=pixel_test)
+
+        # pixel_test2 = {'x': 10, 'y': 20, 'r': 1, 'g': 1, 'b': 1}
         # display.pixel_on(pixel=pixel_test2)
 
-        for i in xrange(1, 20):
-            for j in xrange(1, 10):
-                display.pixel_on(pixel={'x': j, 'y': i, 'r': 255, 'g': 255, 'b': 255})
+        #for i in xrange(1, 20):
+        #    for j in xrange(1, 10):
+        #        display.pixel_on(pixel={'x': j, 'y': i, 'r': 255, 'g': 255, 'b': 255})
     except KeyboardInterrupt:
         print 'User Interrupt, stopping'
         run = False
